6573e071005f324a891d5949e340bf3fc7a4ae53
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2019-02-06 18:11:28 +0000

    Update export_png_manual_fromROI.m

--- IFCB_tools/export_png_manual_fromROI.m
+++ IFCB_tools/export_png_manual_fromROI.m
@@ -1,8 +1,8 @@
 %USER SET PATHS
 %where are your manual classification results? same resultpath as for manual_classify
-resultpath = '\\sosiknas1\IFCB_products\IFCB102_PiscesNov2014\Manual_fromClass\';
-outputpath = '\\sosiknas1\IFCB_products\IFCB102_PiscesNov2014\ROI_images\'; %USER where to write out pngs
-roibasepath = '\\sosiknas1\IFCB_data\IFCB102_PiscesNov2014\data\xxxx\'; %USER where are your ROIs, put xxxx to mark loaction so of year digits
+resultpath = 'F:\IFCB104\manual\'; % manual annotation file location
+outputpath = 'F:\IFCB104\manual\png\'; %USER where to write out pngs
+roibasepath =  'F:\IFCB104\data\XXXX\'; %USER where are your ROIs, put XXXX to mark loaction so of year digits
 %urlbase = 'http://ifcb-data.whoi.edu/mvco/'; %USER where is your dashboard\web server
 
 resultfilelist = dir([resultpath 'D*.mat']);
@@ -15,36 +15,38 @@ for filecount = 1:length(resultfilelist),
 
     %USER CHOOSE A LINE AND EDIT FOR YOUR CASE
     %category = class2use_manual; %use this syntax to export ALL categories
-    category = {   'Ciliate_mix'
-    'Didinium_sp'
-    'Euplotes_sp'
-    'Laboea_strobila'
-    'Leegaardiella_ovalis'
-    'Mesodinium_sp'
-    'Pleuronema_sp'
-    'Strobilidium_morphotype1'
-    'Strombidium_capitatum'
-    'Strombidium_conicum'
-    'Strombidium_inclinatum'
-    'Strombidium_morphotype1'
-    'Strombidium_morphotype2'
-    'Strombidium_oculatum'
-    'Strombidium_wulffi'
-    'Tiarina_fusus'
-    'Tintinnid'
-    'Tontonia_appendiculariformis'
-    'Tontonia_gracillima'
-    'Eutintinnus'
-    'Favella'
-    'Helicostomella_subulata'
-    'Stenosemella_sp1'
-    'Stenosemella_sp2'
-    'Tintinnidium'
-    'Tintinnopsis'
-    'Balanion_sp'
-    'other'}; %use this syntax to export ONLY the listed categories
+%     category = {   'Ciliate_mix'
+%     'Didinium_sp'
+%     'Euplotes_sp'
+%     'Laboea_strobila'
+%     'Leegaardiella_ovalis'
+%     'Mesodinium_sp'
+%     'Pleuronema_sp'
+%     'Strobilidium_morphotype1'
+%     'Strombidium_capitatum'
+%     'Strombidium_conicum'
+%     'Strombidium_inclinatum'
+%     'Strombidium_morphotype1'
+%     'Strombidium_morphotype2'
+%     'Strombidium_oculatum'
+%     'Strombidium_wulffi'
+%     'Tiarina_fusus'
+%     'Tintinnid'
+%     'Tontonia_appendiculariformis'
+%     'Tontonia_gracillima'
+%     'Eutintinnus'
+%     'Favella'
+%     'Helicostomella_subulata'
+%     'Stenosemella_sp1'
+%     'Stenosemella_sp2'
+%     'Tintinnidium'
+%     'Tintinnopsis'
+%     'Balanion_sp'
+%     'other'}; %use this syntax to export ONLY the listed categories
     %category = setdiff(class2use_manual, {'bad' 'ciliate' 'detritus'});  %use this syntax to export all EXCEPT the listed categories
-    %category = setdiff(class2use_manual, {'other' 'misc_nano'});  %use this syntax to export all EXCEPT the listed categories
+    category = setdiff(class2use_manual, {'Centric<10' 'unclassified' ...
+        'Beads' 'bubbles' 'Ash_glassy' 'Ask_dark', 'Detritus'...
+        'Alexandrium_doublet' 'Alexandrium_triplet' 'Alexandrium_quad'});  %use this syntax to export all EXCEPT the listed categories
 
     disp(resultfile)
     %make subdirs for tiffs
@@ -55,10 +57,10 @@ for filecount = 1:length(resultfilelist),
     end;
     if resultfile(1) == 'D'
         roipath = [roibasepath resultfile(1:9) filesep];
-        roipath = regexprep(roipath, 'xxxx', resultfile(2:5));
+        roipath = regexprep(roipath, 'XXXX', resultfile(2:5));
     else
         roipath = [roibasepath resultfile(1:14) filesep];
-        roipath = regexprep(roipath, 'xxxx', resultfile(7:10));
+        roipath = regexprep(roipath, 'XXXX', resultfile(7:10));
     end;
     
     %loop over classes and save pngs to subdirs
