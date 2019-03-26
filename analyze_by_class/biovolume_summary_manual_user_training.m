d0678f4bfb0562505e59373841a0c8e5ea995893
Alexis Fischer <31672503+alexisfischer@users.noreply.github.com>
2019-03-26 23:24:12 +0000

    update biovolume for CA

--- analyze_by_class/biovolume_summary_manual_user_training.m
+++ analyze_by_class/biovolume_summary_manual_user_training.m
@@ -1,13 +1,20 @@
-resultpath = 'C:\work\IFCB\user_training_test_data\manual\'; %USER
-roibasepath = 'C:\work\IFCB\user_training_test_data\data\'; %USER
-feapath = 'C:\work\IFCB\user_training_test_data\features\2014\'; %USER
+function [ ] = biovolume_summary_manual_user_training( manualpath, datapath, feapath_base, summary_dir)
+%function [ ] = biovolume_summary_manual_user_training( manualpath, datapath, feapath_base, summary_dir)
+%
+% Heidi M. Sosik, Woods Hole Oceanographic Institution, September 2012 / August 2015
+% modified by Alexis D. Fischer, UCSC, January 2019
+% 
+% manualpath = 'F:\IFCB104\manual\'; %USER manual file location
+% datapath = 'F:\IFCB104\data\'; %USER where to access data (hdr files) (url for web services, full path for local)
+% feapath_base = 'F:\IFCB104\features\XXXX\'; %USER
+% summary_dir = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SCW\Data\IFCB_summary\manual\'; %where summary file goes
 micron_factor = 1/3.4; %USER PUT YOUR OWN microns per pixel conversion
-filelist = dir([resultpath 'D*.mat']);
+filelist = dir([manualpath 'D*.mat']);
 
 %calculate date
 matdate = IFCB_file2date({filelist.name});
 
-load([resultpath filelist(1).name]) %read first file to get classes
+load([manualpath filelist(1).name]) %read first file to get classes
 numclass = length(class2use_manual);
 class2use_manual_first = class2use_manual;
 classcount = NaN(length(filelist),numclass);  %initialize output
@@ -17,19 +24,19 @@ ml_analyzed = NaN(length(filelist),1);
 for filecount = 1:length(filelist),
     filename = filelist(filecount).name;
     disp(filename)
-    hdrname = [roibasepath filesep filename(2:5) filesep filename(1:9) filesep regexprep(filename, 'mat', 'hdr')]; 
+    hdrname = [datapath filesep filename(2:5) filesep filename(1:9) filesep regexprep(filename, 'mat', 'hdr')]; 
     ml_analyzed(filecount) = IFCB_volume_analyzed(hdrname);
      
-    load([resultpath filename])
+    load([manualpath filename])
   %  yr = str2num(filename(7:10));
     clear targets
-  %  feapath = regexprep(feapath_base, 'XXXX', filename(7:10));
+    feapath = regexprep(feapath_base, 'XXXX', filename(2:5));
     [~,file] = fileparts(filename);
     feastruct = importdata([feapath file '_fea_v2.csv'], ',');
     ind = strmatch('Biovolume', feastruct.colheaders);
     targets.Biovolume = feastruct.data(:,ind);
     ind = strmatch('roi_number', feastruct.colheaders);
-    tind = feastruct.data(:,ind);
+    tind = feastruct.data(:,ind);   
     
     classlist = classlist(tind,:);
     if ~isequal(class2use_manual, class2use_manual_first)
@@ -50,11 +57,12 @@ for filecount = 1:length(filelist),
 end;
 
 class2use = class2use_manual_first;
-if ~exist([resultpath 'summary\'], 'dir')
-    mkdir([resultpath 'summary\'])
-end;
-datestr = date; datestr = regexprep(datestr,'-','');
+
+%datestr = date; datestr = regexprep(datestr,'-','');
 notes = 'Biovolume in units of cubed micrometers';
-save([resultpath 'summary\count_biovol_manual_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'classbiovol', 'filelist', 'class2use', 'notes')
+save([summary_dir 'count_biovol_manual'], 'matdate', 'ml_analyzed', 'classcount', 'classbiovol', 'filelist', 'class2use', 'notes')
+
+disp('Summary biovolume file stored here:')
+disp([summary_dir 'count_biovol_manual'])
 
 
--- analyze_by_class/countcells_manual_user_training.m
+++ analyze_by_class/countcells_manual_user_training.m
@@ -1,30 +1,30 @@
-function [ ] = countcells_manual_user_training( resultpath, in_dir, summary_dir)
-%function [ ] = countcells_manual_user_training( resultpath, in_dir, summary_dir)
+function [ ] = countcells_manual_user_training( manualpath, datapath, summary_dir)
+%function [ ] = countcells_manual_user_training( manualpath, datapath, summary_dir)
 %For example:
 % Heidi M. Sosik, Woods Hole Oceanographic Institution, September 2012 / August 2015
 % modified by Alexis D. Fischer, UCSC, January 2019
 %
 %Example inputs:
-%resultpath = 'F:\IFCB104\manual\'; %USER manual file location
-%in_dir = 'F:\IFCB104\data\'; %USER where to access data (hdr files) (url for web services, full path for local)
+%manualpath = 'F:\IFCB104\manual\'; %USER manual file location
+%datapath = 'F:\IFCB104\data\'; %USER where to access data (hdr files) (url for web services, full path for local)
 %summary_dir = 'C:\Users\kudelalab\Documents\GitHub\bloom-baby-bloom\SCW\Data\IFCB_summary\manual\'; %where summary file goes
 
 % summarizes class results for a series of manual annotation files (as saved by startMC)
 
 %make sure input paths end with filesep
-if ~isequal(resultpath(end), filesep)
-    resultpath = [resultpath filesep];
+if ~isequal(manualpath(end), filesep)
+    manualpath = [manualpath filesep];
 end
-if ~isequal(in_dir(end), filesep)
-    in_dir = [in_dir filesep];
+if ~isequal(datapath(end), filesep)
+    datapath = [datapath filesep];
 end
 
-filelist = dir([resultpath 'D*.mat']);
+filelist = dir([manualpath 'D*.mat']);
 
 %calculate date
 matdate = IFCB_file2date({filelist.name});
 
-load([resultpath filelist(1).name]) %read first file to get classes
+load([manualpath filelist(1).name]) %read first file to get classes
 numclass = length(class2use_manual);
 class2use_manual_first = class2use_manual;
 classcount = NaN(length(filelist),numclass);  %initialize output
@@ -32,10 +32,10 @@ ml_analyzed = NaN(length(filelist),1);
 for filecount = 1:length(filelist),
     filename = filelist(filecount).name;
     disp(filename)
-    hdrname = [in_dir filesep filename(2:5) filesep filename(1:9) filesep regexprep(filename, 'mat', 'hdr')]; 
+    hdrname = [datapath filesep filename(2:5) filesep filename(1:9) filesep regexprep(filename, 'mat', 'hdr')]; 
     ml_analyzed(filecount) = IFCB_volume_analyzed(hdrname);
      
-    load([resultpath filename])
+    load([manualpath filename])
     if ~isequal(class2use_manual, class2use_manual_first)
         [t,ii] = min([length(class2use_manual_first), length(class2use_manual)]);
         if ~isequal(class2use_manual(1:t), class2use_manual_first(1:t)),
@@ -55,10 +55,10 @@ end;
 class2use = class2use_manual_first;
 
 datestr = date; datestr = regexprep(datestr,'-','');
-save([summary_dir 'count_manual_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'filelist', 'class2use')
+save([summary_dir 'count_class_manual_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'filelist', 'class2use')
 
 disp('Summary cell count file stored here:')
-disp([summary_dir 'count_manual_' datestr])
+disp([summary_dir 'count_class_manual_' datestr])
 
 return
 
